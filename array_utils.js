e7d707643877ab91ecb7d95c846fe0bbe559ba42
Ryan Williams <ryan.blake.williams@gmail.com>
2014-06-01 01:29:29 -0400

    wrap JS for client-side use as well, bump to 0.0.4

--- array_utils.js
+++ array_utils.js
@@ -1,41 +1,43 @@
 
-function init(context) {
+(function(exports) {
+  function init(context) {
 
     context = context || this;
-    require('./namespace').namespace('Array.prototype', context);
+    require('namespace').namespace('Array.prototype', context);
 
-    context.Array.prototype.find = function(fn) {
-        for (var i = 0; i < this.length; ++i) {
-            if (fn(this[i]))
-                return this[i];
-        }
-        return null;
+    context.Array.prototype.find = function (fn) {
+      for (var i = 0; i < this.length; ++i) {
+        if (fn(this[i]))
+          return this[i];
+      }
+      return null;
     };
 
-    context.Array.prototype.addEach = function(k, v) {
-        this.forEach(function(e) {
-            e[k] = v;
-        });
-        return this;
+    context.Array.prototype.addEach = function (k, v) {
+      this.forEach(function (e) {
+        e[k] = v;
+      });
+      return this;
     };
 
-    context.Array.prototype.exists = function(fn) {
-        for (var i = 0; i < this.length; ++i) if (fn(this[i])) return true;
-        return false;
+    context.Array.prototype.exists = function (fn) {
+      for (var i = 0; i < this.length; ++i) if (fn(this[i])) return true;
+      return false;
     };
 
-    context.Array.prototype.forall = function(fn) {
-        for (var i = 0; i < this.length; ++i) if (!fn(this[i])) return false;
-        return true;
+    context.Array.prototype.forall = function (fn) {
+      for (var i = 0; i < this.length; ++i) if (!fn(this[i])) return false;
+      return true;
     };
 
-    context.Array.prototype.sum = function() {
-        return this.reduce(function(x,y) { return x+y; });
+    context.Array.prototype.sum = function () {
+      return this.reduce(function (x, y) {
+        return x + y;
+      });
     };
-}
+  }
 
-init();
+  init();
 
-if ((typeof module !== 'undefined') && (typeof module.exports !== 'undefined')) {
   exports.init = init;
-}
+})((typeof module !== 'undefined' && typeof module.exports !== 'undefined') ? module.exports : (this['util/img-mapping/foreground@2x'] = {}));
--- namespace.js
+++ namespace.js
@@ -1,12 +1,15 @@
 
-exports.namespace = function(ns, context) {
+(function(exports) {
+
+  exports.namespace = function(ns, context) {
 
     ns.split('.').reduce(
-        function(ctx, segment) {
+          function(ctx, segment) {
             ctx[segment] = ctx[segment] || {};
             return ctx[segment];
-        },
-            context || this
+          },
+                context || this
     )
-}
+  };
 
+})((typeof module !== 'undefined' && typeof module.exports !== 'undefined') ? module.exports : (this['namespace'] = {}));
